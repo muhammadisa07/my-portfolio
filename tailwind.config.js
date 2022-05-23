71f6e7e93a377f2156c3dddc05abc048c77efcac
muhammadisa07 <muhammadisa0704@gmail.com>
2022-05-24 01:56:18 +0700

    done

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ['./index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#1e40af',
+        secondary: '#64748b',
+        dark: '#0f172a',
+        darkprimary: '#3b82f6',
+        darksecondary: '#ffffff',
+      },
+      screens: {
+        '2xl': '1320px',
+
+      },
+    },
+  },
+  plugins: [],
+};
